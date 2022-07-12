cc41ba37506e2a42502c25db8ede7daf6bcbd68b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-07-12 11:11:10 +0530

    Add DB Connection

---
+++ src/db/DBConnection.java
@@ -0,0 +1,28 @@
+package db;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dbConnection = null;
+    private final Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/IJSE", "root", "1234");
+    }
+
+    public static DBConnection getDbConnection() throws SQLException, ClassNotFoundException {
+        return dbConnection == null ? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
