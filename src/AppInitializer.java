2f123e1d12fc42b05e91f20a2c42f03f7703a538
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-07-12 08:58:28 +0530

    Add MVC Pakeges and AppInitializer

---
+++ src/AppInitializer.java
@@ -0,0 +1,25 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+    }
+}
