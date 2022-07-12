2a56857735e8302681f6d5533bdf104b51e2b0c3
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-07-12 09:42:18 +0530

    Add Model Pakage add Student Model

---
+++ src/model/Student.java
@@ -0,0 +1,22 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private String studentId;
+    private String studentName;
+    private String email;
+    private String contact;
+    private String address;
+    private String nic;
+
+}
